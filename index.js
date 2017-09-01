c65db2d16c40e8dc01e6df21e259c155966c3306
Marcelo Ferreira <joamarcelo@gmail.com>
2017-09-01 08:02:19 -0300

    Adds clustering

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 import app from './config/custom-express';
+import cluster from "cluster";
+import os from "os";
 
 const PORT = 3000;
+const CPUS = os.cpus();
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+if (cluster.isMaster) {
+    CPUS.forEach(cluster.fork);
+
+    cluster.on('exit', worker => {
+        console.log(`Worker ${worker.process.pid} died`);
+        cluster.fork();
+    });
+} else {
+    console.log(`Starting worker ${process.pid}`);
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
